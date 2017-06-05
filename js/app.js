3e265e62556d1cbba825da1a68adc94695e1deaa
dmitrypashkov <dmitry.pashkov@nextiva.com>
2017-06-05 18:16:28 +0300

    Basic functionality

--- js/app.js
+++ js/app.js
@@ -1,46 +1,104 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var ROW_HEIGHT = 80;
+var SECOND_ROW_Y = 60;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var THIRD_ROW_Y = SECOND_ROW_Y + ROW_HEIGHT;
+var FOURTH_ROW_Y = THIRD_ROW_Y + ROW_HEIGHT;
+var ENEMY_ROWS_Y_VALUES = [SECOND_ROW_Y, THIRD_ROW_Y, FOURTH_ROW_Y];
+var PLAYER_SKINS = [
+    "images/char-boy.png",
+    "images/char-cat-girl.png",
+    "images/char-horn-girl.png",
+    "images/char-pink-girl.png",
+    "images/char-princess-girl.png"
+];
+var ENEMY_ATTACK_AREA = 50;
+
+//ENEMY CLASS
+var Enemy = function (speed) {
+    this.x = 0;
+    this.initRandomY();
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
+Enemy.prototype.update = function (dt) {
+    this.x += this.speed * dt;
+    if (this.x > 500) {
+        this.x = -100;
+        this.initRandomY();
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    function checkLooseScenario() {
+        if ((this.x - player.x <= ENEMY_ATTACK_AREA && this.x - player.x >= -ENEMY_ATTACK_AREA)
+            && this.y === player.y) {
+            startNewGame();
+        }
+    }
+
+    checkLooseScenario.call(this);
 };
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.initRandomY = function () {
+    this.y = ENEMY_ROWS_Y_VALUES[Math.floor(Math.random() * ENEMY_ROWS_Y_VALUES.length)];
+};
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+//PLAYER CLASS
+var Player = function () {
+    this.x = 200;
+    this.y = 300;
+};
+Player.prototype.update = function () {
+    if (this.currentMove === "left") {
+        this.x -= 100;
+    } else if (this.currentMove === "right") {
+        this.x += 100;
+    } else if (this.currentMove === "up") {
+        this.y -= 80;
+    } else if (this.currentMove === "down") {
+        this.y += 80;
+    }
+    this.currentMove = "";
 
+    function checkWinScenario() {
+        if (this.y < 60) {
+            startNewGame();
+        }
+    }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+    checkWinScenario.call(this);
+};
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.handleInput = function (key) {
+    this.currentMove = key;
+};
 
+//START A NEW GAME LOGIC
+var allEnemies;
+var player;
+var startNewGame = function () {
+    allEnemies = [];
+    for (var i = 0; i < Math.floor(Math.random() * 10) + 3; i++) {
+        allEnemies.push(new Enemy(Math.floor(Math.random() * 500) + 100))
+    }
+    player = new Player();
+    player.currentMove = "";
+    player.sprite = PLAYER_SKINS[Math.floor(Math.random() * PLAYER_SKINS.length)];
+};
+startNewGame();
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+//LISTENERS
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
--- js/engine.js
+++ js/engine.js
@@ -159,7 +159,6 @@ var Engine = (function(global) {
      * those sorts of things. It's only called once by the init() method.
      */
     function reset() {
-        // noop
     }
 
     /* Go ahead and load all of the images we know we're going to need to
@@ -171,7 +170,12 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        "images/enemy-bug.png",
+        "images/char-cat-girl.png",
+        "images/char-horn-girl.png",
+        "images/char-pink-girl.png",
+        "images/char-princess-girl.png"
     ]);
     Resources.onReady(init);
 
